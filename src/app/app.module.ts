4f78370f41e74029e1592f127bc6bd77b3e3e19b
xyntechx <xyntechx@gmail.com>
2021-11-08 20:24:50 +0800

    ✨ New components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlayComponent } from './play/play.component';
+import { QuizComponent } from './quiz/quiz.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PlayComponent, QuizComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
