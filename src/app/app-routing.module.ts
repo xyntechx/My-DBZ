51e4d72784b60422f5fec3c2f6bab9504c4d9f6b
xyntechx <xyntechx@gmail.com>
2021-11-08 21:06:07 +0800

    ✨ Add Routing: Quiz

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QuizComponent } from './quiz/quiz.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'quiz', component: QuizComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
