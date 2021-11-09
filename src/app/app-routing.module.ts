17a2644e383838ca0ddace5d3cee873285b34c23
xyntechx <xyntechx@gmail.com>
2021-11-09 22:15:05 +0800

    ✨ how-to-play path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { QuizComponent } from './quiz/quiz.component';
 import { AppComponent } from './app.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { HowToPlayComponent } from './how-to-play/how-to-play.component';
 
 const routes: Routes = [
   { path: '', component: AppComponent },
   { path: 'quiz', component: QuizComponent },
+  { path: 'how-to-play', component: HowToPlayComponent },
 ];
 
 @NgModule({
