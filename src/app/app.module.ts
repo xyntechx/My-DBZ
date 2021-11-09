c7622f500ace54f5e53d7eaeaed5ee493a43af9a
xyntechx <xyntechx@gmail.com>
2021-11-09 22:16:07 +0800

    ✨ New components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlayComponent } from './play/play.component';
 import { QuizComponent } from './quiz/quiz.component';
+import { HowToPlayComponent } from './how-to-play/how-to-play.component';
+import { TitleComponent } from './title/title.component';
 
 @NgModule({
-  declarations: [AppComponent, PlayComponent, QuizComponent],
+  declarations: [
+    AppComponent,
+    PlayComponent,
+    QuizComponent,
+    HowToPlayComponent,
+    TitleComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
