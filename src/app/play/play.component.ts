6202c02d7eefcdc916967bec3310400c873e22b0
xyntechx <xyntechx@gmail.com>
2021-11-09 19:40:11 +0800

    ✨ Pass props

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-play',
   templateUrl: './play.component.html',
-  styleUrls: ['./play.component.scss']
+  styleUrls: ['./play.component.scss'],
 })
 export class PlayComponent implements OnInit {
+  @Input() question = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
