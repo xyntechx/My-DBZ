1d089385b151b353c113d16da9af46da834f4649
xyntechx <xyntechx@gmail.com>
2021-11-11 14:44:11 +0800

    🔥 Remove /results path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { QuizComponent } from './quiz/quiz.component';
 import { HowToPlayComponent } from './how-to-play/how-to-play.component';
-import { ResultsComponent } from './results/results.component';
 
 const routes: Routes = [
   { path: '', component: AppComponent },
   { path: 'quiz', component: QuizComponent },
   { path: 'how-to-play', component: HowToPlayComponent },
-  { path: 'results', component: ResultsComponent },
 ];
 
 @NgModule({
