94e5fcc4032de9fe352c0954ed856504705e3ccb
xyntechx <xyntechx@gmail.com>
2021-11-11 15:01:39 +0800

    ✨ Complete Results Component

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
-  styleUrls: ['./results.component.scss']
+  styleUrls: ['./results.component.scss'],
 })
 export class ResultsComponent implements OnInit {
+  @Input() character = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
