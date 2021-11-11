3b2490d3d9e0f439dbbb8eafb2cb61f9587e186d
xyntechx <xyntechx@gmail.com>
2021-11-11 15:01:18 +0800

    ✨ Complete Play component

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-play',
@@ -6,9 +6,41 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./play.component.scss'],
 })
 export class PlayComponent implements OnInit {
-  @Input() question = '';
+  @Input() characters = {};
+  @Input() index = 0;
+
+  @Output() newIndex = new EventEmitter<number>();
+
+  updateIndex() {
+    this.newIndex.emit();
+  }
+
+  answers: string[] = [];
+  character: string = '';
+
+  appendAnswer(newAnswer: string) {
+    this.answers.push(newAnswer);
+    this.character = findMaxCount(this.answers);
+  }
 
   constructor() {}
 
   ngOnInit(): void {}
 }
+
+const findMaxCount = (array: string[]) => {
+  const maxValue = array.reduce((previous, current, i, arr) => {
+    if (
+      arr.filter((item) => item === previous).length >
+      arr.filter((item) => item === current).length
+    ) {
+      return previous;
+    } else {
+      return current;
+    }
+  });
+
+  console.log(maxValue);
+
+  return maxValue;
+};
